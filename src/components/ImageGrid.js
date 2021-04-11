548de79a114ff6950b50448192ab924e5edb6447
kishanhitk <kishansharma1231@gmail.com>
2021-04-11 14:36:28 +0530

    ✨ Add image grid

---
+++ src/components/ImageGrid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import useFirestore from '../hooks/useFirestore'
+
+export default function ImageGrid() {
+    const { docs } = useFirestore('images');
+    console.log(docs);
+    return (
+        <div className='img-grid'>
+            {docs && docs.map(doc => {
+                return <div className='img-wrap' key={doc.id}>
+                    <img src={doc.url} alt={doc.url}></img>
+                </div>
+            })}
+        </div>
+    )
+}
